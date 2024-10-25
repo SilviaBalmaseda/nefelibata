7f540e361cd355e11550e87cf0e29c9e9394bf71
SilviaBalmaseda <99995278+SilviaBalmaseda@users.noreply.github.com>
2024-10-25 21:08:05 +0200

    Update DaoFavorito.php

--- models/DaoFavorito.php
+++ models/DaoFavorito.php
@@ -14,14 +14,14 @@ class DaoFavorito
     // Crear favorito.
     public function createFavorite($usuarioId, $historiaId)
     {
-        $stmt = $this->db->prepare("INSERT INTO Favorito (UsuarioId, HistoriaId) VALUES (?, ?)");
+        $stmt = $this->db->prepare("INSERT INTO favorito (UsuarioId, HistoriaId) VALUES (?, ?)");
         return $stmt->execute([$usuarioId, $historiaId]);
     }
 
     // Devuelve true(1) si existe o false(0) si no.
     public function esFavorito($usuarioId, $historiaId)
     {
-        $stmt = $this->db->prepare("SELECT COUNT(*) FROM Favorito WHERE UsuarioId = ? AND HistoriaId = ?");
+        $stmt = $this->db->prepare("SELECT COUNT(*) FROM favorito WHERE UsuarioId = ? AND HistoriaId = ?");
         $stmt->execute([$usuarioId, $historiaId]);
         return $stmt->fetchColumn() > 0;
     }
@@ -30,7 +30,7 @@ class DaoFavorito
     // Borrar capítulo con ese id pasado.
     public function deleteFavorite($usuarioId, $historiaId)
     {
-        $stmt = $this->db->prepare("DELETE FROM Favorito WHERE UsuarioId = ? AND HistoriaId = ?");
+        $stmt = $this->db->prepare("DELETE FROM favorito WHERE UsuarioId = ? AND HistoriaId = ?");
         return $stmt->execute([$usuarioId, $historiaId]);
     }
 }
